15ab5d7d566428586e32e2484ac4d1061135046e
Joss <281900360@qq.com>
2025-05-20 08:02:57 +0800

    add mac wal commnad

---
+++ flon.chain/node-wal/run-fuwal-mac-ver.sh
@@ -0,0 +1,26 @@
+
+mkdir -p ~/.fuwal
+
+git clone git@github.com:fullon-labs/flon-docker.git
+
+cd flon-docker
+cp ./flon.env ~/flon.env
+#修改 ~/flon.evn set NET mainnet
+export NET=mainnet
+
+cd flon-docker/flon.chain/node-wal
+./run-fuwal.sh ~/.fuwal
+
+
+# 也可以执行一下命令运行 Docker 容器
+cd ~/.fuwal
+docker run -d \
+  --name fuwal \
+  --workdir /opt/flon \
+  --entrypoint ./bin/run-wallet.sh \
+  -v "$(pwd)":/opt/flon \
+  -v "$(pwd)/bin/.bashrc":/root/.bashrc \
+  -v "$(pwd)/bin-script":/root/bin \
+  -v /Users/joss/code/workspace:/workspace \
+  ghcr.io/fullon-labs/floncore/funod:0.5.0 \
+  .
