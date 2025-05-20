e79f9c02f41bf6460da1951e976e48a64fa0cc99
Joss <281900360@qq.com>
2025-05-20 15:13:51 +0800

    update install cdt shell script

--- flon.chain/node-wal/install-cdt.sh
+++ flon.chain/node-wal/install-cdt.sh
@@ -3,15 +3,13 @@
 #docker exec -it fuwal bash
 
 cd /opt/contracts/
+apt update
 apt install -y git
 curl -o flon.cdt.deb https://flon-test.oss-cn-hongkong.aliyuncs.com/deb/flon.cdt_0.5.0-1_amd64.deb
 
 git clone https://github.com/fullon-labs/toolkit.contracts.git
 git clone https://github.com/fullon-labs/flon.contracts.git
 
-
-
-apt update
 apt install -y libssl-dev libboost-all-dev libgmp3-dev libbz2-dev libreadline-dev libncurses5-dev libusb-1.0-0-dev libudev-dev libusb-dev libusb-1.0-0
 apt install libcurl4-gnutls-dev cmake -y
 apt install -y g++ libz3-dev
